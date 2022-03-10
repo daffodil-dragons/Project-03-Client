14121cf7c81985800bf3bd19bbcceee77167575f
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-09 16:11:33 -0800

    finished styling on about us page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,15 +1,16 @@
 //dependencies
 import React  from "react";
-import aboutUsData from "./aboutUsData"
+import aboutUsData from "./aboutUsData";
 import './AboutUs.css';
 
 function AboutUs() {
     let teammates = aboutUsData.map((person, key) => {
         return (
             <div className="member">
-                <h1>{person.name}</h1>
-                <div>{person.github}</div>
-                {person.linkedIn}
+                <h1 className="person">{person.name}</h1>
+                <p>{person.text}</p>
+                <a href={person.github} className="github">Github</a>
+                <a href={person.linkedIn} className="linkedIn">LinkedIn</a>
             </div>
         )
     })
--- src/components/AboutUs/aboutUsData.js
+++ src/components/AboutUs/aboutUsData.js
@@ -1,16 +1,19 @@
 let aboutUsData = [
     {
-        name: "Daniel Ribuffo", 
+        name: "Daniel Ribuffo",
+        text: "", 
         github: "https://github.com/dribuffo",
         linkedIn: "https://www.linkedin.com/in/danielribuffo"
     },
     {
         name: "Austin Cesear", 
+        text: "",
         github: "github",
         linkedIn: "linkedIn"
     },
     {
         name: "Mitch Bennett", 
+        text: "",
         github: "https://github.com/mrb5691",
         linkedIn: "https://www.linkedin.com/in/mitchell-r-bennett/"
     }
